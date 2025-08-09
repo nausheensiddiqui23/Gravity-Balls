c73de10099742a3b1605d2d349d31d21965f71d1
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-09 21:03:07 +0530

    new commit

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const gravity = 0.5; // Gravity force
+const friction = 0.8; // Bounce damping
+const colors = ['#ff4757', '#1e90ff', '#2ed573', '#ffa502', '#ff6b81'];
+
