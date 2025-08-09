8270a267157aab3e1ccfbf2698003d37a3fa4330
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-09 21:03:42 +0530

    new commit

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const gravity = 0.5; // Gravity force
 const friction = 0.8; // Bounce damping
 const colors = ['#ff4757', '#1e90ff', '#2ed573', '#ffa502', '#ff6b81'];
 
+class Ball {
+  constructor(x, y, radius, color, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.dx = dx; // horizontal velocity
+    this.dy = dy; // vertical velocity
+  }
